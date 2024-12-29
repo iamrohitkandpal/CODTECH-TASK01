807ff54072b62f6adf2f6ab56ae2c4f41de71558
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-12-29 23:38:51 +0530

    All the structure of the whole backend of the project completed with all the installed packages

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import { connectDB } from './lib/db.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT: " + PORT);
+    connectDB();
+})
